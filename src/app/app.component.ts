b6bb0de6c119922b4e439e2b9b788db54b7cad28
Krym <gumimash@gmail.com>
2024-02-25 20:19:04 +0200

    observable test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RoomService } from './room.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-project';
+
+  constructor(private roomService: RoomService) {
+    this.roomService.getRooms().subscribe((val) => {
+      console.log(val);
+    });
+  }
 }
